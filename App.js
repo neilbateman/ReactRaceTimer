03ffc65532eb7bd713efea54f7131fbb0d1ce42c
Student <example@example.com>
2019-09-30 12:44:11 -0700

    constructing the constructor, lifecycle methdos

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timer: null,
+            min_Counter: '00',
+            sec_Counter: '00',
+            startDisable: false
+        }
+    }
+    componentWillUnmount() {
+        clearInterval(this.state.timer)
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+    render(){  
+        return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    });
+    }
