cb09d977892d3d8565f8ba0ab46a31a42d2a04e2
Student <example@example.com>
2019-09-30 12:58:08 -0700

    create onButtonStart method

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
+    
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
 export default class App extends React.Component {
     constructor(props) {
         super(props);
@@ -14,23 +24,30 @@ export default class App extends React.Component {
     componentWillUnmount() {
         clearInterval(this.state.timer)
     }
-
+    onButtonStart = () => {
+        let timer = setInterval(() => {
+            var num = (Number(this.state.sec_Counter) +1 ).toString(),
+                count = this.state.min_Counter;
+            if (Number(this.state.sec_Counter) == 59 )  {
+                count = (Number(this.state.min_Counter) + 1).toString();
+                num = '00'
+            } 
+            this.setState({
+                min_Counter: count.length == 1 ? '0' + count : count,
+                sec_Counter: num.length == 1 ? '0' + num : num
+            });
+        }, 1000);
+        this.setState({ timer });
+        this.setState({startDisable : true})
+    }
 
 
     render(){  
         return (
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Open up App.js to start alsdfjals;dfjlasdfj on your app!</Text>
         </View>
       );
     }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    });
+
     }
